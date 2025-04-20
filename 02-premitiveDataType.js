66e27e562d7bb029aebc3ab12931c27f6a1db8f6
hsabhi <hsabhi@gmail.com>
2025-04-20 11:09:52 +0530

    Add files via upload

---
+++ 02-premitiveDataType.js
@@ -0,0 +1,17 @@
+let firstName = "Anindya";  
+console.log("firstName:", firstName, "| Type:", typeof firstName);
+
+
+let companyName = "EclecticIQ";  
+console.log("companyName:", companyName, "| Type:", typeof companyName);
+
+let mobileNumber = 9876543210;
+console.log("mobileNumber:", mobileNumber, "| Type:", typeof mobileNumber);
+
+
+let isAutomation = true;       
+console.log("isAutomation:", isAutomation, "| Type:", typeof isAutomation);
+
+let hasPlaywright;                    
+console.log("hasPlaywright:", hasPlaywright, "| Type:", typeof hasPlaywright);
+
