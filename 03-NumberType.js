04798d7f4506e33c71e863d5921d766a9c0e6b43
hsabhi <hsabhi@gmail.com>
2025-04-20 11:21:53 +0530

    Add files via upload
    added 3rd assignment

---
+++ 03-NumberType.js
@@ -0,0 +1,16 @@
+let number = 0;
+checkNumber(number)
+
+function checkNumber(number) {
+
+    if (number > 0) {
+     console.log("the number is positive: "+number)
+     
+    } else if (number < 0) {
+      console.log("the number is negative: "+number)
+      
+    } else {
+      console.log("the number is neutral: "+number);
+    }
+  }
+  
